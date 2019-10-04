b8aee5964763f5783afda4223a4437c210cd2dfa
dirkh <githubdirk24@gmx.net>
2019-10-04 16:56:10 +0200

    Image prediction added

--- app.py
+++ app.py
@@ -3,10 +3,19 @@
 from scripts import tabledef
 from scripts import forms
 from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
 import json
 import sys
 import os
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, render_template, request, session
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)  # Generic key for dev purposes only
@@ -16,6 +25,26 @@ app.secret_key = os.urandom(12)  # Generic key for dev purposes only
 # Heroku environment variable configurations for Flask.
 #from flask_heroku import Heroku
 #heroku = Heroku(app)
+
+from keras.applications.resnet50 import ResNet50
+
+
+# ======== Helper functions =========================================================== #
+# -------- predict an image ----------------------------------------------------------- #
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
 
 # ======== Routing =========================================================== #
 # -------- Login ------------------------------------------------------------- #
@@ -79,6 +108,30 @@ def settings():
         user = helpers.get_user()
         return render_template('settings.html', user=user)
     return redirect(url_for('login'))
+
+# -------- upload ---------------------------------------------------------- #
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    model = ResNet50(weights='imagenet')
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['image']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
 
 
 # ======== Main ============================================================== #
---
+++ static/js/main.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
+
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                console.log("onload");
+                var image = new Image();
+                image.src = e.target.result;
+                image.onload = function (imageEvent) {
+                    console.log("image onload");
+                    console.log("Width: " + this.width);
+                    console.log("Height: " + this.height);
+
+                    var c = document.getElementById("myCanvas");
+                    //const width = 600;
+                    console.log("$(window).width(): " + $(window).width())
+                    console.log("$(window).width(): " + $(window).width())
+                    console.log("window.screen.width: " + window.screen.width)
+                    console.log("window.screen.availWidth: " + window.screen.availWidth)
+                    console.log("$(document).width(): " + $(document).width())
+
+                    //const width = $(window).width()/2;
+                    //const width = window.screen.width/2;
+                    const width = window.screen.availWidth / 2;
+                    const scaleFactor = width / image.width;
+
+                    //c.width = this.width;
+                    c.width = width
+                    c.height = this.height * scaleFactor;
+                    var ctx = c.getContext("2d");
+                    $('#imagePreview').css("width", width);
+                    $('#imagePreview').css("height", this.height * scaleFactor);
+                    //ctx.drawImage(image, 0, 0, this.width, this.height);
+                    ctx.drawImage(image, 0, 0, width, this.height * scaleFactor);
+                    $('.img-preview').css("width", width);
+                    $('.img-preview').css("height", this.height * scaleFactor);
+                }
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+                
+            }
+            reader.readAsDataURL(input.files[0]);
+            
+        }
+    }
+    $("#imageUpload").change(function () {
+        console.log("change");
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
+
+});
